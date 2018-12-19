a61367f390085bad117152b66201b3e93d3e8bec
Eric Wilson <ericswilson94@gmail.com>
2018-12-18 16:55:01 -0800

    Removed more dead code from app.js

--- ReactMusicTest/App.js
+++ ReactMusicTest/App.js
@@ -6,50 +6,13 @@ import shorthash from 'shorthash';
 
 const socketServer = require('socket.io-client')('http://localhost:3003');
 
-// const serverData = [
-//   {
-//     id: 1,
-//     title: 'Stressed Out',
-//     artist: 'Twenty One Pilots',
-//     albumArtUrl: "http://36.media.tumblr.com/14e9a12cd4dca7a3c3c4fe178b607d27/tumblr_nlott6SmIh1ta3rfmo1_1280.jpg",
-//     audioUrl: 'http://localhost:8080/tune3.mp3'
-//   },
-//   {
-//     id: 2,
-//     title: 'Iron Lion Zion',
-//     artist: 'Bob Marley',
-//     albumArtUrl: "http://36.media.tumblr.com/14e9a12cd4dca7a3c3c4fe178b607d27/tumblr_nlott6SmIh1ta3rfmo1_1280.jpg",
-//     audioUrl: 'http://localhost:8080/tune4.mp3'
-//   }
-// ]
-
-// function asyncRequest() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(serverData)
-//     }, 3000)
-//   })
-// }
-
 export default class App extends Component {
-
-  // constructor(props){
-  //   super(props)
-  //   this.
     
-    state = {
-      tracks: [
-        // {
-        //   id: 1,
-        //   title: '',
-        //   artist: '',
-        //   albumArtUrl: '',
-        //   audioUrl: ''
-        // }
-      ],
-      loading: true,
-      socket: socketServer
-    }
+  state = {
+    tracks: [],
+    loading: true,
+    socket: socketServer
+  }
 
   loadFile = ( id, path )=> {
     const index = this.state.tracks.findIndex(track => track.id === id)
@@ -68,23 +31,19 @@ export default class App extends Component {
     }, () => console.log(this.state.tracks))
   }
 
-
   downloadFile = (id, uri,path) => {
-
     RNFS.downloadFile({fromUrl:uri, toFile: path}).promise
-        .then(res => {
-          if(res.statusCode === 200) { 
-            this.loadFile(id, path)
-          } else {
-            console.log(res.statusCode)
-          }
-        });
+      .then(res => {
+        if(res.statusCode === 200) { 
+          this.loadFile(id, path)
+        } else {
+          console.log(res.statusCode)
+        }
+      });
   }
 
   componentDidMount(){
-    // Wait three seconds, then create a tracks object from the response data passed by the function
-    // Fill that tracks object with each track, and add a 'null' local file to each track
-    
+
       const ship1 = "http://localhost:8080/ships/1";
     
       const getTracks = new Promise((resolve, reject) => {
@@ -133,18 +92,13 @@ export default class App extends Component {
       });
 
 
-    this.state.socket.on('connect', function(){
-      console.log("Client side");
-    });
-
-    this.state.socket.on('message', function(data){
-      console.log(data);
+    this.state.socket.on('connect', client => {
+      console.log("Client connected");
 
       this.state.socket.on('disconnect', function(){
         console.log("Client disconnected");
       });
-
-    });
+    })
   }
 
   render() {
